cff0d94804ba3d0507164dacf35496f5f17dbe2f
soniejka <ivalkou@gmail.com>
2015-02-19 14:48:03 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo ">>>>> Building project..."
 hugo
 cd public
+echo ">>>>> GIT Processing..."
 git add -A
-git commit -a -m "Update: `date`"
+git commit -a -m "Update: `date +"%Y-%d-%m %H:%M:%S %z"`"
 git push --force
 cd ..
-echo "Done"
+echo ">>>>> Done!"
